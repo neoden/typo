ebc804755015fd5d90e0aa649cf707dc24eb01de
neoden <lenar.imamutdinov@gmail.com>
2016-03-06 15:09:17 +0300

    backup script some parameters moved to config

--- backup.sh
+++ backup.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
 BACKUP_DIR=/var/backup
-APP_NAME=typo
-DB_NAME=typo
-DB_USER=typo
-DAYS_TO_KEEP_LOCAL_BACKUPS=7
 DATE=$(eval date +%Y%m%d)
+
+function check_param() {
+    local name=$1
+    local param=${!name}
+    if [ -z "${param}" ]; then
+        echo "${name} is not set"
+        exit 1
+    fi
+}
+
+source $1
+
+check_param "HOOK_URL"
+check_param "STORAGE_USER"
+check_param "STORAGE_KEY"
+check_param "STORAGE_DIR"
+check_param "APP_NAME"
+
+if [ -z "${DB_NAME}" ]; then DB_NAME=${APP_NAME}; fi
+if [ -z "${DB_USER}" ]; then DB_USER=${DB_NAME}; fi
+if [ -z "${DB_PASSWORD}" ]; then DB_PASSWORD=; fi
+if [ -z "${DAYS_TO_KEEP_LOCAL_BACKUPS}" ]; then DAYS_TO_KEEP_LOCAL_BACKUPS=7; fi
+
 LOG_DIR=/var/log/${APP_NAME}
 LOG=${LOG_DIR}/${DB_NAME}_backup.log
 
@@ -36,22 +55,6 @@ then
     echo "Config file not specified"
     exit 1
 fi
-
-source $1
-
-function check_param() {
-    local name=$1
-    local param=${!name}
-    if [ -z "${param}" ]; then
-        echo "${name} is not set"
-        exit 1
-    fi
-}
-
-check_param "HOOK_URL"
-check_param "STORAGE_USER"
-check_param "STORAGE_KEY"
-check_param "STORAGE_DIR"
 
 CURRENT_BACKUP_FILENAME=${BACKUP_DIR}/${DB_NAME}-${DATE}.tar.gz
 
